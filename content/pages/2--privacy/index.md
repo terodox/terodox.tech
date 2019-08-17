e8a5cb0b4e30e1faa2ef462a53ff0838b15a7b3b
terodox <terodox@gmail.com>
2019-08-17 08:34:47 -0400

    Update landing page message

--- content/pages/2--privacy/index.md
+++ content/pages/2--privacy/index.md
@@ -3,3 +3,4 @@ title: Privacy Policy
 ---
 
 Please see our full privacy policy [here](https://app.termly.io/document/privacy-policy/a079d3db-aab6-4147-88fc-4d0d6e52c5ab).
+
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,7 +10,7 @@ const Hero = props => {
     <React.Fragment>
       <section className="hero">
         <h1>
-          This is a demo site of&nbsp;the <strong>heroBlog</strong> GatsbyJS starter
+          A site of technical musings, and the occasional rant.
         </h1>
         <button onClick={scrollToContent} aria-label="scroll">
           <FaArrowDown />
