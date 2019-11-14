7d6cd1a3b0ae5c452a2c88875d172a78995937d1
terodox <terodox@gmail.com>
2019-11-14 18:56:15 -0500

    Another attempt at codefund

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -6,20 +6,10 @@ const Article = props => {
 
   return (
     <React.Fragment>
-      <div id="codefund"></div>
-      <script src="https://codefund.io/properties/541/funder.js" async="async"></script>
       <article className="article">{children}</article>
 
       {/* --- STYLES --- */}
       <style jsx>{`
-        #codefund {
-          min-height: 100px;
-          display: block !important;
-          visibility: visible !important;
-          background-color: red;
-          padding: ${theme.space.inset.default};
-          margin: 0 auto;
-        }
         .article {
           padding: ${theme.space.inset.default};
           margin: 0 auto;
@@ -28,18 +18,10 @@ const Article = props => {
           .article {
             max-width: ${theme.text.maxWidth.tablet};
           }
-          #codefund {
-            max-width: ${theme.text.maxWidth.tablet};
-            padding: ${`calc(${theme.space.default}) calc(${theme.space.default} * 2)`};
-          }
         }
         @from-width desktop {
           .article {
             max-width: ${theme.text.maxWidth.desktop};
-          }
-          #codefund {
-            max-width: ${theme.text.maxWidth.desktop};
-            padding: ${`calc(${theme.space.default} * 2 + 90px) 0 calc(${theme.space.default} * 2)`};
           }
         }
       `}</style>
