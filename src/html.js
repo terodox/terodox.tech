7764907d98db2d4c288adff29e327645efbe713e
Andrew Desmarais <terodox@gmail.com>
2019-11-14 19:21:13 -0500

    Add CodeFund placeholder and script (#350)

--- src/html.js
+++ src/html.js
@@ -34,6 +34,9 @@ export default class HTML extends React.Component {
           {this.props.preBodyComponents}
           <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
           {this.props.postBodyComponents}
+
+          <div id="codefund"></div>
+          <script src="https://codefund.io/properties/541/funder.js" async="async"></script>
         </body>
       </html>
     );
